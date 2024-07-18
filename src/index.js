4cb8e5e6f905251418a1618221ae4cd019950646
Monoblade <157546326+Monobladegg@users.noreply.github.com>
2024-07-18 16:03:16 +0300

    init backend

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+import express from "express";
+import cors from "cors";
+import { register, login, getMe } from "./controllers/auth";
+import { registrationValidator, loginValidator } from "./validators/auth";
+import handleValidationsErrors from "./utils/handleValidationsErrors";
+import checkAuth from "./middleware/checkAuth";
+
+mongoose
+  .connect(
+    "mongodb+srv://Monoblade:<password>@maincluster.y9ksfln.mongodb.net/?retryWrites=true&w=majority&appName=MainCluster"
+  )
+  .then(() => {
+    console.log("MongoDB connected");
+  })
+  .catch((err) => {
+    console.log("MongoDB connection error: ", err);
+  });
+
+const app = express()
+
+app.use(express.json())
+app.use(cors({
+  origin: "http://localhost:3000",
+  credentials: true
+}))
+
+app.post("/api/auth/register", registrationValidator, handleValidationsErrors, register)
+app.post("/api/auth/login", loginValidator, handleValidationsErrors, login)
+app.get("/api/auth/me", checkAuth, getMe)
+
+
+app.listen(5000, () => console.log("Server started on port 5000"))
